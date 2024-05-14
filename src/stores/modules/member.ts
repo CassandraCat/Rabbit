97a587bb7cfb2a496bba0f1c49d39bd0eabcfe36
CrazyCatZhang <crazycatzhang@outlook.com>
2024-05-14 20:51:10 +0800

    feat: pinia presist state for applet

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -27,6 +27,18 @@ export const useMemberStore = defineStore(
   },
   // TODO: 持久化
   {
-    persist: true,
+    // H5
+    // persist: true,
+    // Applet
+    persist: {
+      storage: {
+        getItem(key) {
+          return uni.getStorageSync(key)
+        },
+        setItem(key, value) {
+          uni.setStorageSync(key, value)
+        },
+      },
+    },
   },
 )
