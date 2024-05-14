6c56d206642cecc7a21562aac42ed81c10d3b8a4
CrazyCatZhang <crazycatzhang@outlook.com>
2024-05-14 20:37:53 +0800

    feat: initial uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
-  <view class="index">index</view>
+  <uni-card
+    title="基础卡片"
+    sub-title="副标题"
+    extra="额外信息"
+    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+  >
+    <text>...... .. you</text>
+  </uni-card>
+
+  <view class="index">111122222111</view>
 </template>
 
 <style lang="scss">
